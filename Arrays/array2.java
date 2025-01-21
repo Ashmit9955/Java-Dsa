bd4e95b54d4637d25bdaa57c08ce2342ebf67bf1
Ashmit9955 <ashmit9955@gmail.com>
2025-01-21 07:08:53 +0000

    testing rm --cached

--- Arrays/array2.java
+++ Arrays/array2.java
@@ -8,6 +8,9 @@ public class array2 {
             marks[i] = marks[i] + 1;
         }
     }
+
+
+    
     public static void main(String args[]){
         int marks[] = {97,98,99};
         for(int i =0; i < marks.length ; i++){
